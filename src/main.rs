4e1a9ee08f3ea47893c71a8a7cd9e94a8a4623c4
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-08-08 16:03:14 +0800

    feat: test env

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ async fn main() {
 
 
   // run our app with hyper, listening globally on port 3000
-  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:4090").await.unwrap();
   axum::serve(listener, app).await.unwrap();
 }
 
