acb0ed9b236b985bc754c26a1cdffc515b252345
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-08-15 10:52:31 +0800

    feat: listen_and_send optimise

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,19 +1,14 @@
+use eyre::Result;
 use serde::{Deserialize, Serialize};
-use std::error::Error;
 
-mod following_order;
-mod tg_user;
-mod trading_order;
+
 pub mod param_models;
 
-// pub fn convert<'a, From: Serialize, To:  Deserialize<'a> >(from: &From) -> Result<To, Box<dyn Error>> {
-//   let serialised = serde_json::to_string(from)?;
-//   let result = serde_json::from_str::<To>(&serialised);
-//   match result {
-//     Ok(to) => { Ok(to) }
-//     Err(e) => { Err(AppError::new_box("as")) }
-//   }
-// }
+pub fn convert<From: Serialize, To: for<'a> Deserialize<'a>>(from: &From) -> Result<To> {
+  let serialised = serde_json::to_string(from)?;
+  let to = serde_json::from_str::<To>(&serialised)?;
+  Ok(to)
+} 
 
 
 
--- src/domain/tg_user.rs
+++
@@ -1,7 +0,0 @@
-// use diesel::{Queryable, Selectable};
-
-// tg_user
-// Created by julian on 2024-07-31.
-// #[derive(Queryable, Selectable)]
-// #[diesel(table_name = crate::schema::posts)]
-// #[diesel(check_for_backend(diesel::pg::Pg))]
