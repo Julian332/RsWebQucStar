f3693eadc70bb7114d20baf5ba08f41d75d5b13f
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-10-14 11:25:53 +0800

    feature: derive page macro

---
+++ derive_macro/derive_builder/tests/dummy-crate/src/lib.rs
@@ -0,0 +1,21 @@
+/// This dummy crate serves as a playground to inspect things like generated documentation
+
+#[macro_use]
+extern crate derive_builder;
+
+// re-export Foo and FooBuilder to manually check for documentation consistency
+// https://github.com/colin-kiegel/rust-derive-builder/issues/46
+pub use internal::Foo as Bar;
+pub use internal::FooBuilder as BarBuilder;
+mod internal {
+    #[derive(Builder)]
+    pub struct Foo {
+        pub a: i32,
+    }
+}
+
+#[test]
+fn test() {
+    let x = BarBuilder::default().a(42).build().unwrap();
+    println!("{}", x.a);
+}
