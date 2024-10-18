fdb9472bbb74991fb93d5d2f62ba3e15b418d556
jinguocao332@gmail.com <jinguocao332@gmail.com>
2024-10-18 09:27:51 +0800

    op

--- src/api_doc/mod.rs
+++ src/api_doc/mod.rs
@@ -33,7 +33,7 @@ pub fn empty_resp_docs(op: TransformOperation) -> TransformOperation {
 pub fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
     api.title("RsWebQuicStar")
         .summary("RsWebQuicStar")
-        .description(include_str!("../README.md"))
+        .description(include_str!("../Doc.md"))
         .tag(Tag {
             name: "todo".into(),
             description: Some("Todo Management".into()),
